bd92a43df96868625a642c14120c85ed8af1a4a1
sebastian <sebastian.ulbel@gmail.com>
2016-03-06 23:30:09 +0100

    updated screenshots updated portfolio infos updated portfolio styling

--- components/scripts/portfolio.js
+++ components/scripts/portfolio.js
@@ -184,7 +184,7 @@ var imagesLoaded = require('imagesloaded');
         this.current = this.items.indexOf(item);
 
         // set the src of the original image element (large image)
-        this._setOriginal(item.querySelector('a').getAttribute('href'));
+        this._setOriginal(item.querySelector('span').getAttribute('data-link'));
 
         // callback
         this.options.onOpenItem(this, item);
