8aeaae579270a03c8028769b00fe97813244d097
sebastian <sebastian.ulbel@gmail.com>
2016-03-14 22:50:08 +0100

    added some bottom spacing on main section removed fullPageScrolling due to browser incompatibility adjusted menu to new scrolling removed template.js renamed page1 and page1 to home and portfolio removed height from portfolio changed margin from % to rem due to browser compatibility

--- builds/development/js/fullPageScrolling.js
+++ builds/development/js/fullPageScrolling.js
@@ -5,75 +5,75 @@ $(function() {
     $(".more").on('click',function() {
         //maybe try changing animate to velocity
         $('html, body').animate({
-            scrollTop: $('#page2').offset().top
+            scrollTop: $('#portfolio').offset().top
         }, 500);
         return false;
     });
 });
 
 //fullPageScrolling
-var pages = 2;
-var currentpage = 1;
-if (document.location.hash) { currentpage = parseInt(document.location.hash.replace('#', '')); }
-
-var nextpage = currentpage + 1; if (nextpage > pages) { nextpage = pages; }
-var prevpage = currentpage - 1; if (prevpage < 1) { prevpage = 1; }
-
-var animatingup = false;
-var animatingdown = false;
-
-$(document).ready(function() {
-    resizeDiv();
-});
-
-window.onresize = function(event) {
-    resizeDiv();
-    scrolltocurrent();
-}
-
-$(window).scroll(function(event) {
-
-    if (animatingup==true) { return; }
-    if (animatingdown==true) { return; }
-
-    nextpage = currentpage + 1; if (nextpage > pages) { nextpage = pages; }
-    prevpage = currentpage - 1; if (prevpage < 1) { prevpage = 1; }
-
-    if (animatingup == false) {
-        if ($(window).scrollTop()+$(window).height()>=$("#page"+(nextpage)).offset().top+50) {
-            if (nextpage > currentpage) {
-                var p2 = $( "#page"+(nextpage) );
-                var pageheight = p2.position().top;
-                animatingdown = true;
-                $('html, body').animate({ scrollTop: pageheight }, 500, function() { currentpage = nextpage; animatingdown = false; document.location.hash = currentpage;});
-                return;
-            }
-        }
-    }
-
-    if (animatingdown == false) {
-        if ($(window).scrollTop()<=$("#page"+(currentpage)).offset().top-50) {
-            if (prevpage < currentpage) {
-                var p2 = $( "#page"+(currentpage) );
-                var pageheight = p2.position().top-$(window).height();
-                animatingup = true;
-                $('html, body').animate({ scrollTop: pageheight }, 500, function() { currentpage = prevpage; animatingup = false; document.location.hash = currentpage;});
-                return;
-            }
-        }
-    }
-});
-
-
-function scrolltocurrent() {
-    var p2 = $( "#page"+(currentpage) );
-    var pageheight = p2.position().top;
-    $('html, body').animate({ scrollTop: pageheight }, 200);
-}
-
-function resizeDiv() {
-    vpw = $(window).width();
-    vph = $(window).height();
-    $('.page').css({'min-height': vph + 'px'});
-}
+//var pages = 2;
+//var currentpage = 1;
+//if (document.location.hash) { currentpage = parseInt(document.location.hash.replace('#', '')); }
+//
+//var nextpage = currentpage + 1; if (nextpage > pages) { nextpage = pages; }
+//var prevpage = currentpage - 1; if (prevpage < 1) { prevpage = 1; }
+//
+//var animatingup = false;
+//var animatingdown = false;
+//
+//$(document).ready(function() {
+//    resizeDiv();
+//});
+//
+//window.onresize = function(event) {
+//    resizeDiv();
+//    scrolltocurrent();
+//}
+//
+//$(window).scroll(function(event) {
+//
+//    if (animatingup==true) { return; }
+//    if (animatingdown==true) { return; }
+//
+//    nextpage = currentpage + 1; if (nextpage > pages) { nextpage = pages; }
+//    prevpage = currentpage - 1; if (prevpage < 1) { prevpage = 1; }
+//
+//    if (animatingup == false) {
+//        if ($(window).scrollTop()+$(window).height()>=$("#page"+(nextpage)).offset().top+50) {
+//            if (nextpage > currentpage) {
+//                var p2 = $( "#page"+(nextpage) );
+//                var pageheight = p2.position().top;
+//                animatingdown = true;
+//                $('html, body').animate({ scrollTop: pageheight }, 500, function() { currentpage = nextpage; animatingdown = false; document.location.hash = currentpage;});
+//                return;
+//            }
+//        }
+//    }
+//
+//    if (animatingdown == false) {
+//        if ($(window).scrollTop()<=$("#page"+(currentpage)).offset().top-50) {
+//            if (prevpage < currentpage) {
+//                var p2 = $( "#page"+(currentpage) );
+//                var pageheight = p2.position().top-$(window).height();
+//                animatingup = true;
+//                $('html, body').animate({ scrollTop: pageheight }, 500, function() { currentpage = prevpage; animatingup = false; document.location.hash = currentpage;});
+//                return;
+//            }
+//        }
+//    }
+//});
+//
+//
+//function scrolltocurrent() {
+//    var p2 = $( "#page"+(currentpage) );
+//    var pageheight = p2.position().top;
+//    $('html, body').animate({ scrollTop: pageheight }, 200);
+//}
+//
+//function resizeDiv() {
+//    vpw = $(window).width();
+//    vph = $(window).height();
+//    $('.page').css({'min-height': vph + 'px'});
+//}
 },{}]},{},[1])
