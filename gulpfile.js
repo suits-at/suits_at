ece76c47be070ad21893f346e6b2d39ecaae4119
suits-at <office@suits.at>
2016-03-23 20:30:28 +0100

    added uglify changed impressum content added align-center to base.css due to missing statement after reinstalling pc

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ var env,
     sassStyle;
 
 env = 'production';
-//env = process.env.NODE_ENV || 'development';
+// env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
     outputDir = 'builds/development/';
@@ -81,7 +81,6 @@ gulp.task('jsIndex', function () {
 });
 
 //sourcemap = (environment == :production) ? false : true
-//todo: testen ob autoprefixer funktioniert
 
 gulp.task('compass', function () {
     return gulp.src(sassSources)
