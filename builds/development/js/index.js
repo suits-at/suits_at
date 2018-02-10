7d29d8206e57bab9f93bd33dbb484f0223ee056f
suits-at <office@suits.at>
2018-02-10 14:04:57 +0100

    add new projects to portfolio make some minor changes

--- builds/development/js/index.js
+++ builds/development/js/index.js
@@ -1053,8 +1053,8 @@ $(function() {
 //calling typed.js
 $(function() {
     $(".typed").typed({
-        strings: ["WEB DEVELOPER^3000", "FRONTEND DEVELOPER^3000", "JS DEVELOPER^3000", "PHP DEVELOPER^3000", "WORDPRESS DEVELOPER^3000", "TYPO3 DEVELOPER^3000", "METEOR DEVELOPER^3000"],
-        typeSpeed: 100,
+      strings: ["Sebastian Ulbel IT-Solutions^3000","WEB DEVELOPER^3000", "FRONTEND DEVELOPER^3000", "JS DEVELOPER^3000", "PHP DEVELOPER^3000", "WORDPRESS DEVELOPER^3000", "VUE.JS DEVELOPER^3000"],
+      typeSpeed: 100,
         loop: true,
         showCursor: true
     });
--- components/scripts/defer.js
+++ components/scripts/defer.js
@@ -26,7 +26,7 @@ $(function() {
 //typed js effect
 $(function() {
     $(".typed").typed({
-        strings: ["WEB DEVELOPER^3000", "FRONTEND DEVELOPER^3000", "JS DEVELOPER^3000", "PHP DEVELOPER^3000", "WORDPRESS DEVELOPER^3000", "TYPO3 DEVELOPER^3000", "METEOR DEVELOPER^3000"],
+      strings: ["Sebastian Ulbel IT-Solutions^3000","WEB DEVELOPER^3000", "FRONTEND DEVELOPER^3000", "JS DEVELOPER^3000", "PHP DEVELOPER^3000", "WORDPRESS DEVELOPER^3000", "VUE.JS DEVELOPER^3000"],
         typeSpeed: 100,
         loop: true,
         showCursor: true
